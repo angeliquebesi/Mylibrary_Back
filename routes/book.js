17ac3151d596d7073f57200a13e021b085a9be6b
angeliquebesi <besineaud85@gmail.com>
2022-02-03 12:00:31 +0100

    routes one book ok

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const findAll = require('../models/findAll');
+const findOne = require('../models/findOne');
 
-router.get('/', (req, res) => {
+router.get('/', (res) => {
   findAll()
     .then((result) => {
       res.json(result);
@@ -9,6 +10,12 @@ router.get('/', (req, res) => {
     .catch((err) => {
       console.error(err);
     });
+});
+
+router.get('/:id', (req, res) => {
+  findOne(req.params.id)
+    .then((result) => res.json(result))
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
